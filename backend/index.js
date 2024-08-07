5dc89b898c23177c90b7fd1cf7e05379e0733ec7
manojsingh620 <singhkum684@gmail.com>
2024-08-07 08:14:03 +0530

    header change

--- backend/index.js
+++ backend/index.js
@@ -14,6 +14,8 @@ app.use(bodyParser.json());
 app.use(
   cors({
     origin: 'https://qr-code-scanner-frontend.vercel.app',
+    methods: 'GET,POST,OPTIONS',
+    allowedHeaders: 'Content-Type,Authorization',
     credentials: true,
   })
 );
